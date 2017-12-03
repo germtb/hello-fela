15ce0980f67bca7b2ba7f5dd44c2d2206a69f3f5
Gerard Moreno-Torres Bertran <germtf@gmail.com>
2017-12-03 12:52:13 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createRenderer } from 'fela'
+import { Provider } from 'react-fela'
+
+import App from './App'
+
+const renderer = createRenderer()
+
+ReactDOM.render(
+	<Provider renderer={renderer}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
